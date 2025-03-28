68cdd048a6c932cc0fe1ffdc8de391450014ffe7
reira-endo <103876343+reira-endo@users.noreply.github.com>
2025-03-28 11:27:44 +0900

    TranslationResultコンポーネントから音声再生関連のボタンを削除し、ページ内の音声再生テキストを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const HomePage = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414z" clipRule="evenodd" />
           </svg>
-          {isPlayingAudio ? '再生中...' : '音声再生'}
+          {isPlayingAudio ? '再生中...' : '翻訳音声再生'}
         </button>
       </div>
     );
